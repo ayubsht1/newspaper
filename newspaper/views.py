e7516119007cb552c5b1b6b0c10006ffef9e35c0
Ayub Shrestha <136778245+ayubsht1@users.noreply.github.com>
2024-09-08 08:13:28 +0545

    creating listview and home page named base.html

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from newspaper.models import Post
 # Create your views here.
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'aznews/home.html'
+    context_object_name = "posts"
+    queryset = Post.objects.filter(
+        published_at__isnull=False, status="active"
+    ).order_by("-published_at")[:5]
