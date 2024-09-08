16074478ca628bba06b84461ef3a03a9141d0527
Ayub Shrestha <136778245+ayubsht1@users.noreply.github.com>
2024-09-08 08:39:45 +0545

    dividing a templete into header footer and home and including it to base.html

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from newspaper.models import Post,Category,Tag
 # Register your models here.
+#admin.site.register(Post)
+#admin.site.register(Category)
+#admin.site.register(Tag)
 admin.site.register([Post, Category, Tag])
