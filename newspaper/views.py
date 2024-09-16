83e68176004ba5011209ea5eff590c2ada1ae844
Ayub Shrestha <ayubsht1@gmail.com>
2024-09-16 20:41:57 +0545

    minor fixes

--- newspaper/views.py
+++ newspaper/views.py
@@ -11,12 +11,13 @@ from datetime import timedelta
 
 class HomeView(ListView):
     model = Post
-    template_name = "aznews/home.html"
+    template_name = 'aznews/home.html'
     context_object_name = "posts"
     queryset = Post.objects.filter(
         published_at__isnull=False, status="active"
     ).order_by("-published_at")[:5]
-
+    
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["featured_post"] = (
@@ -25,25 +26,18 @@ class HomeView(ListView):
             .first()
         )
         context["featured_posts"] = Post.objects.filter(
-        published_at__isnull=False, status="active"
-        ).order_by("-published_at", "-views_count")[1:4]
-
+            published_at__isnull=False, status="active"
+            ).order_by("-published_at", "-views_count")[1:4]
+        
         one_week_ago = timezone.now() - timedelta(days=7)
         context["weekly_top_posts"] = Post.objects.filter(
             published_at__isnull=False, status="active", published_at__gte=one_week_ago
-        ).order_by("-published_at", "-views_count")[:7]
-
-
+            ).order_by("-published_at", "-views_count")[:7]
+        
         context["recent_posts"] = Post.objects.filter(
             published_at__isnull=False, status="active"
         ).order_by("-published_at")[:7]
-
-        context['tags'] = Tag.objects.all()[:12]
-        context['categories'] = Category.objects.all()[:4]
-
-        context["trending_posts"] = Post.objects.filter(
-            published_at__isnull=False, status="active"
-        ).order_by("-views_count")[:3]
+        
         return context
     
 class AboutView(TemplateView):
